5a50f6a72bd0832a648162df141cbd99e5f819db
Konrad Kołakowski <vashpan@gmail.com>
2018-02-11 22:47:40 +0100

    Initial Commit

---
+++ XcodeCleaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeCleaner
+//
+//  Created by Konrad Kołakowski on 11.02.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeCleaner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeCleaner
+//
+//  Created by Konrad Kołakowski on 11.02.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
