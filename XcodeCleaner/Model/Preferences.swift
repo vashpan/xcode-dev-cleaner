94521624adc75370e67de01a366ad277394e34e3
Konrad Kołakowski <vashpan@gmail.com>
2018-05-03 21:08:33 +0200

    Added Preferences model, a wrapper to application settings

---
+++ XcodeCleaner/Model/Preferences.swift
@@ -0,0 +1,81 @@
+//
+//  Preferences.swift
+//  XcodeCleaner
+//
+//  Created by Konrad Kołakowski on 01.05.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+
+import Foundation
+
+final class Preferences {
+    // MARK: Types
+    public enum NotificationsPeriod: Int {
+        case every2weeks, everyMonth, every2Months
+    }
+    
+    // MARK: Properties & constants
+    public static let shared = Preferences()
+    
+    private let notificationsEnabledKey = "NotificationsEnabledKey"
+    private let notificationsPeriodKey = "NotificationsPeriodKey"
+    private let dryRunEnabledKey = "DryRunEnabledKey"
+    
+    // MARK: Initialization
+    public init() {
+        
+    }
+    
+    // MARK: Options
+    public var notificationsEnabled: Bool {
+        get {
+            guard UserDefaults.standard.object(forKey: notificationsEnabledKey) != nil else {
+                return false // default value
+            }
+            
+            return UserDefaults.standard.bool(forKey: notificationsEnabledKey)
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue, forKey: notificationsEnabledKey)
+        }
+    }
+    
+    public var notificationsPeriod: NotificationsPeriod {
+        get {
+            guard UserDefaults.standard.object(forKey: notificationsPeriodKey) != nil else {
+                return .everyMonth
+            }
+            
+            let periodInt = UserDefaults.standard.integer(forKey: notificationsPeriodKey)
+            
+            guard let period = NotificationsPeriod(rawValue: periodInt) else {
+                return .everyMonth
+            }
+            
+            return period
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue.rawValue, forKey: notificationsPeriodKey)
+        }
+    }
+    
+    public var dryRunEnabled: Bool {
+        get {
+            guard UserDefaults.standard.object(forKey: dryRunEnabledKey) != nil else {
+                #if DEBUG
+                return true // default value
+                #else
+                return false
+                #endif 
+            }
+            
+            return UserDefaults.standard.bool(forKey: dryRunEnabledKey)
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue, forKey: dryRunEnabledKey)
+        }
+    }
+}
