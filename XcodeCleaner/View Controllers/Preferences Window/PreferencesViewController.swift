403c7bcfca0ed66b46065cb0947f2d6d241f26af
Konrad Kołakowski <vashpan@gmail.com>
2018-05-03 21:47:13 +0200

    Saving & loading preferences from Preferences window

--- XcodeCleaner/View Controllers/Preferences Window/PreferencesViewController.swift
+++ XcodeCleaner/View Controllers/Preferences Window/PreferencesViewController.swift
@@ -15,9 +15,87 @@ final class PreferencesViewController: NSViewController {
     
     @IBOutlet private weak var dryRunEnabledButton: NSButton!
     
+    // MARK: Initialization & overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: load preferences/set defaults
+        // load preferences
+        self.setNotificationsEnabled(Preferences.shared.notificationsEnabled)
+        self.setNotificationsPeriod(Preferences.shared.notificationsPeriod)
+        self.setDryRunEnabled(Preferences.shared.dryRunEnabled)
+    }
+    
+    // MARK: Helpers
+    private func titleFromPeriod(_ period: Preferences.NotificationsPeriod) -> String {
+        let result: String
+        switch period {
+            case .every2weeks:
+                result = "Every 2 weeks"
+            case .everyMonth:
+                result = "Every month"
+            case .every2Months:
+                result = "Every 2 months"
+        }
+        
+        return result
+    }
+    
+    private func periodFromTitle(_ title: String) -> Preferences.NotificationsPeriod? {
+        let result: Preferences.NotificationsPeriod?
+        switch title {
+            case "Every 2 weeks":
+                result = .every2weeks
+            case "Every month":
+                result = .everyMonth
+            case "Every 2 months":
+                result = .every2Months
+            default:
+                result = nil
+        }
+        
+        return result
+    }
+    
+    private func setNotificationsEnabled(_ value:  Bool) {
+        self.notificationsEnabledButton.state = value ? .on : .off
+        self.notificationsPeriodPopUpButton.isEnabled = value
+    }
+    
+    private func setNotificationsPeriod(_ period: Preferences.NotificationsPeriod) {
+        let periodTitle = self.titleFromPeriod(period)
+        self.notificationsPeriodPopUpButton.selectItem(withTitle: periodTitle)
+    }
+    
+    private func setDryRunEnabled(_ value: Bool) {
+        self.dryRunEnabledButton.state = value ? .on : .off
+    }
+    
+    // MARK: Actions
+    @IBAction func updateNotificationsEnabled(_ sender: NSButton) {
+        let enabled = sender.state == .on
+        
+        self.setNotificationsEnabled(enabled)
+        
+        Preferences.shared.notificationsEnabled = enabled
+    }
+    
+    @IBAction func updatePeriod(_ sender: NSPopUpButton) {
+        guard let selectedItem = sender.selectedItem else {
+            return
+        }
+        
+        guard let selectedPeriod = self.periodFromTitle(selectedItem.title) else {
+            return
+        }
+        
+        Preferences.shared.notificationsPeriod = selectedPeriod
+    }
+    
+    @IBAction func updateDryRun(_ sender: NSButton) {
+        let enabled = sender.state == .on
+        
+        self.setDryRunEnabled(enabled)
+        
+        Preferences.shared.dryRunEnabled = enabled
     }
 }
