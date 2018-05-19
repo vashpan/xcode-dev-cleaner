0bd92cd8c711ec5b94b83629929a4a01b48d1b13
Konrad Kołakowski <vashpan@gmail.com>
2018-05-19 17:15:16 +0200

    Added license information in each source file

--- XcodeCleaner/Model/Preferences.swift
+++ XcodeCleaner/Model/Preferences.swift
@@ -5,6 +5,18 @@
 //  Created by Konrad Kołakowski on 01.05.2018.
 //  Copyright © 2018 One Minute Games. All rights reserved.
 //
+//  XcodeCleaner is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  XcodeCleaner is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with XcodeCleaner.  If not, see <http://www.gnu.org/licenses/>.
 
 import Foundation
 
