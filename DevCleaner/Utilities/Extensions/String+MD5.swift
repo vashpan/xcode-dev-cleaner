9496e251c7b47ec51236bb9e8fdec7bb25fe8775
Konrad Kołakowski <vashpan@gmail.com>
2018-09-16 16:47:34 +0200

    Added String MD5 helper extension

---
+++ DevCleaner/Utilities/Extensions/String+MD5.swift
@@ -0,0 +1,44 @@
+//
+//  String+MD5.swift
+//  DevCleaner
+//
+//  Created by Konrad Kołakowski on 18.03.2018.
+//  Copyright © 2018 One Minute Games. All rights reserved.
+//
+//  DevCleaner is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  DevCleaner is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with DevCleaner.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import Foundation
+import CommonCrypto
+
+public extension String {
+    public var md5: String {
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = CUnsignedInt(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLength = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLength)
+        
+        CC_MD5(str!, strLen, result)
+        
+        let hash = NSMutableString()
+        
+        for i in 0..<digestLength {
+            hash.appendFormat("%02x", result[i])
+        }
+        
+        result.deinitialize(count: 1)
+        
+        return hash as String
+    }
+}
