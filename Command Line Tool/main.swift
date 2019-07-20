7a658f1a97c0709d98fa19d44ec40d5ced220867
Konrad Kołakowski <vashpan@gmail.com>
2019-07-20 14:58:03 +0200

    Added stub for command line tool

---
+++ Command Line Tool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  devcleaner-tool
+//
+//  Created by Konrad Kołakowski on 20/07/2019.
+//  Copyright © 2019 One Minute Games. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
