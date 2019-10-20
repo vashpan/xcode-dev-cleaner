cf22aa1c0c77e19ef2c55a441a86689193f0c07d
Konrad Kołakowski <vashpan@gmail.com>
2019-10-20 23:25:46 +0200

    Improvements to MD5 calculations

--- DevCleaner/Utilities/Extensions/String+MD5.swift
+++ DevCleaner/Utilities/Extensions/String+MD5.swift
@@ -24,20 +24,20 @@ import CommonCrypto
 
 extension String {
     public var md5: String {
-        let str = self.cString(using: String.Encoding.utf8)
-        let strLen = CUnsignedInt(self.lengthOfBytes(using: String.Encoding.utf8))
+        let str = self.cString(using: .utf8)
+        let strLen = CUnsignedInt(self.lengthOfBytes(using: .utf8))
         let digestLength = Int(CC_MD5_DIGEST_LENGTH)
         let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLength)
+        defer {
+            result.deallocate()
+        }
         
         CC_MD5(str!, strLen, result)
         
         let hash = NSMutableString()
-        
         for i in 0..<digestLength {
             hash.appendFormat("%02x", result[i])
         }
-        
-        result.deinitialize(count: 1)
         
         return hash as String
     }
