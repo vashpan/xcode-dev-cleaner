37a1eaacfaee89101493a23bcc2446676f72ba74
Konrad Kołakowski <vashpan@gmail.com>
2019-11-11 23:14:49 +0100

    Fixed issue with not being able to cancel from installing command line tool

--- DevCleaner/Managers/Files.swift
+++ DevCleaner/Managers/Files.swift
@@ -32,16 +32,18 @@ public final class Files {
     public static var commandLineToolFolder: URL = URL(fileURLWithPath: "/usr/local/bin", isDirectory: true)
     
     // MARK: Acquire folder permissions
-    private static func acquireFolderPermissions(folderUrl: URL, openPanelMessage: String? = nil) -> URL? {
+    private static func acquireFolderPermissions(folderUrl: URL, allowCancel: Bool = false, openPanelMessage: String? = nil) -> URL? {
         let message = openPanelMessage ??
                       "DevCleaner needs permission to this folder to scan its contents. Folder should be already selected and all you need to do is to click \"Open\"."
         
         return folderUrl.acquireAccessFromSandbox(bookmark: Preferences.shared.folderBookmark(for: folderUrl),
+                                                  allowCancel: allowCancel,
                                                   openPanelMessage: message)
     }
     
     public static func aqcuireCommandLineToolFolderPermission() -> URL? {
         return acquireFolderPermissions(folderUrl: Files.commandLineToolFolder,
+                                        allowCancel: true,
                                         openPanelMessage: "In order to install command line tool, DevCleaner needs permission to folder where you want to install it. You can choose a different one if you want, but then make sure it will be accessible from your PATH.")
     }
     
